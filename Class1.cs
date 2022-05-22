a5561933fdcc54ce8a3749b9f9868b4ec75de700
Keithmax6 <crazybharathmeetyou@outlook.com>
2022-05-22 22:06:21 +0530

    Add project files.

---
+++ Class1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Text.RegularExpressions;
+
+namespace ClassLibrary
+{
+    public class Class1
+    {
+        public string Uppercase(string str)
+        {
+            string lowerCase;
+            lowerCase = str.ToLower();
+            string s = lowerCase;
+            s = Regex.Replace(s, @"(^\w)|(\s\w)", m => m.Value.ToUpper());
+            return s;
+        }
+
+        public string VowelCount(string s)
+        {
+            int i, len, vowel_count;
+            vowel_count = 0;
+            len = s.Length;
+            for (i = 0; i < len; i++)
+            {
+                if (s[i] == 'a' || s[i] == 'e' || s[i] == 'i' || s[i] == 'o' || s[i] == 'u' || s[i] == 'A' || s[i] == 'E' || s[i] == 'I' || s[i] == 'O' || s[i] == 'U')
+                {
+                    vowel_count++;
+                }
+            }
+            return vowel_count.ToString();
+        }
+
+    }
+}
